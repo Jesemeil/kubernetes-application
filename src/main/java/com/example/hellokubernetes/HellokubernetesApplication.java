2e0f745b99881683a7e1aabd88c5f77c5402220f
Aghogho <otahaghogho@gmail.com>
2025-02-10 12:16:15 +0100

    first commit

---
+++ src/main/java/com/example/hellokubernetes/HellokubernetesApplication.java
@@ -0,0 +1,13 @@
+package com.example.hellokubernetes;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HellokubernetesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HellokubernetesApplication.class, args);
+	}
+
+}
